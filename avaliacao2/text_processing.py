68763f94159ee1152129042dc421f1274b551014
lucaslioli <loliveira@inf.ufsm.br>
2018-07-02 02:25:56 -0300

    Updating the emoji_pattern to remove more than before

--- avaliacao2/text_processing.py
+++ avaliacao2/text_processing.py
@@ -27,7 +27,10 @@ if __name__ == '__main__':
         u"\U0001F300-\U0001F5FF"  # symbols & pictographs
         u"\U0001F680-\U0001F6FF"  # transport & map symbols
         u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
         "]+", flags=re.UNICODE)
+
 
     # Getting the set of stop words
     stop = set(stopwords.words('portuguese'))
