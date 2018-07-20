4a59f406cde9283a3335100d7c4b1f407b923b5f
lucaslioli <loliveira@inf.ufsm.br>
2018-07-20 11:32:08 -0300

    Inserting files used in final work of Digital Images Processing

---
+++ image_processing/addNoise.py
@@ -0,0 +1,22 @@
+import sys
+import numpy as np
+import random
+import cv2
+
+def sp_noise(image,prob):
+    output = np.zeros(image.shape,np.uint8)
+    thres = 1 - prob
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            rdn = random.random()
+            if rdn < prob:
+                output[i][j] = 0
+            elif rdn > thres:
+                output[i][j] = 255
+            else:
+                output[i][j] = image[i][j]
+    return output
+
+image = cv2.imread(sys.argv[1], 0)
+noise_img = sp_noise(image, 0.05)
+cv2.imwrite('sp_noise.jpg', noise_img)
